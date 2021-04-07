31f4e3a7dab80a59fa3820397bcff811750c028a
Kyoko <kyokoconnect@gmail.com>
2021-04-07 01:49:12 -0400

    1st commit, lab complete

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+// const gifts = [Ada, Brendan, Ali];
+//     function writeCards(gifts) {
+//         for(let i = 0, i < gifts.length; i++) {
+//             console.log()
+//         } 
+//     }
+
+
+    function writeCards(namesArray, event) {
+        const messages = []
+        for (let i = 0; i < namesArray.length; i++) {
+        const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+        messages.push(message);
+        }
+    return messages
+    }
+    
+    function countDown(number) {
+        for (let i = number; i >= 0; i--) {
+            console.log(i);
+        }
+    }
